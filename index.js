386f96c5dd59dffff84bc9043bfacf03aea83b37
Geoff Hing <geoffhing@gmail.com>
2016-05-19 22:39:52 -0500

    ALlow configuring port

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var PORT = process.env.ORDER_MOLLY_PORT || 3000;
+
 app.use('/js', express.static('js'));
 app.use('/css', express.static('css'));
 
@@ -36,6 +38,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(PORT, function(){
+  console.log('listening on *:' + PORT);
 });
